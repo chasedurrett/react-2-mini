3a9482c649a4bd93d0518995ba5d580ececed68f
chasedurrett <chase.durrett80@gmail.com>
2020-05-13 13:48:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Components
-import EditToggle from './components/EditToggle';
-import ColorChanger from './components/ColorChanger';
-import SizeChanger from './components/SizeChanger';
-import FamilyChanger from './components/FamilyChanger';
-import TextContainer from './components/TextContainer';
+import EditToggle from "./components/EditToggle";
+import ColorChanger from "./components/ColorChanger";
+import SizeChanger from "./components/SizeChanger";
+import FamilyChanger from "./components/FamilyChanger";
+import TextContainer from "./components/TextContainer";
 
 class App extends Component {
   // constructor
-
+  constructor() {
+    super();
+    this.state = {
+      fontColor: "black",
+      fontSize: 12,
+      fontFamily: "monospace",
+      allowEdit: "true",
+    };
+    this.updateColor = this.updateColor.bind(this);
+    this.updateSize = this.updateSize.bind(this);
+    this.updateFamily = this.updateFamily.bind(this);
+    this.updateEditStatus = this.updateEditStatus(this);
+  }
   // updateColor
+  updateColor(val) {
+    this.setState({
+      fontColor: val,
+    });
+  }
 
   // updateSize
+  updateSize(val) {
+    this.setState({
+      fontSize: val,
+    });
+  }
 
   // updateFamily
+  updateFamily(val) {
+    this.setState({
+      fontFamily: val,
+    });
+  }
 
   // updateEditStatus
+  updateEditStatus(val) {
+    this.setState({
+      allowEdit: val,
+    });
+  }
 
   render() {
+    console.log("state ", this.state);
+    const { allowEdit, fontFamily, fontColor, fontSize } = this.state;
     return (
       <div>
         <div className="headerBar">
-          {/* Render EditToggle */}
-          {/* Render ColorChanger */}
-          {/* Render SizeChanger */}
-          {/* Render FamilyChanger */}
+          <EditToggle update={this.updateEditStatus} />
+          <ColorChanger update={this.updateColor} allowEdit={allowEdit} />{" "}
+          {/* how you pass props */}
+          <SizeChanger update={this.updateSize} allowEdit={allowEdit} />
+          <FamilyChanger update={this.updateFamily} allowEdit={allowEdit} />
         </div>
-        <div className="textArea">{/* Render TextContainer */}</div>
+        <div className="textArea">
+          <TextContainer
+            fontSize={fontSize}
+            fontColor={fontColor}
+            fontFamily={fontFamily}
+          />
+        </div>
       </div>
     );
   }
--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class TextContainer extends Component {
   constructor() {
     super();
     this.state = {
-      text: '',
+      text: "",
     };
+    this.updateText = this.updateText.bind(this);
   }
 
   updateText(e) {
@@ -18,7 +19,11 @@ export default class TextContainer extends Component {
     return (
       <div className="textContainer">
         <textarea
-          // style={  }
+          style={{
+            color: this.props.fontColor,
+            fontFamily: this.props.fontFamily,
+            fontSize: this.props.fontSize,
+          }}
           onChange={this.updateText}
           value={this.state.text}
           placeholder="Start typing your thoughts here!"
